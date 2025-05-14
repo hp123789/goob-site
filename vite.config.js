733a59660ea0b0264fea37bce19b94fa3e07e897
Hamza <hamzaperacha@gmail.com>
2025-05-14 02:07:22 -0700

    vite config name

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './', // This sets the base URL for your project in production
+  base: '/goob-site/', // This sets the base URL for your project in production
   build: {
     rollupOptions: {
       output: {
